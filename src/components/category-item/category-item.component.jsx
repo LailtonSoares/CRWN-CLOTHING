728a9941bcd82129c1a01cde98036b4069ce925c
Lailton Soares <lailsoares@hotmail.com>
2022-10-16 17:42:00 -0300

    added our sign in component as well as firebase utils and brought in

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -3,10 +3,6 @@ import "./category-item.styles.scss";
 const categoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
-
-
-  
-
   return (
     <div className='category-container'>
       <div
